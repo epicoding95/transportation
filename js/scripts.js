e03fff5da377c180ee1ad6c81d0c8ae794052303
kelar crisp and Serhiy Savchuk <kelar@>
2019-06-11 13:52:49 -0700

    finished version of transportation

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,25 @@
-$(document).ready(function() {
-  $("form").submit(function(event() {
+$(document).ready(function(){
+  $("form#transportation_survey").submit(function(event) {
+    event.preventDefault();
+
+    $("#work-responses").show();
+    $("input:checkbox[name=work-transportation]:checked").each(function() {
+
+      var workTransportationMode = $(this).val();
+
+      var upperWorkTransportationMode = workTransportationMode.toUpperCase();
+
+      $("#work-responses").append(upperWorkTransportationMode + "<br>");
+});
+$("#fun-responses").show();
+$("input:checkbox[name=fun-transportation]:checked").each(function(){
+    var funTransportation = $(this).val();
+    var upperFunTransportation = funTransportation.toUpperCase();
+
+        $("#fun-responses").append(upperFunTransportation + "<br>");
 
 
 
-
-    event.preventDefaut();
+    });
   });
 });
